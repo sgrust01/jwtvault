80c64576b2c7652243d14259881f249fa7adf9bc
Saurav Gupta <saurav2k4u@gmail.com>
2020-02-16 14:59:20 +0800

    Added testcases for error module

--- src/errors.rs
+++ src/errors.rs
@@ -38,3 +38,57 @@ pub enum LoginFailed {
     #[fail(display = "{} - Reason: {}", 0, 1)]
     InvalidTokenOwner(String, String),
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn validate_certificate_error() {
+        let err = CertificateError::BadFile("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2");
+
+        let err = CertificateError::FileReadError("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2")
+    }
+
+    #[test]
+    fn validate_token_error() {
+        let err = TokenErrors::TokenEncodingFailed("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2");
+
+        let err = TokenErrors::TokenDecodingFailed("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2");
+
+        let err = TokenErrors::MissingServerRefreshToken("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2");
+
+        let err = TokenErrors::InvalidServerRefreshToken("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2");
+
+        let err = TokenErrors::InvalidClientAuthenticationToken("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2");
+    }
+
+    #[test]
+    fn validate_login_error() {
+        let err = LoginFailed::MissingPassword("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2");
+
+        let err = LoginFailed::InvalidPassword("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2");
+
+        let err = LoginFailed::InvalidTokenOwner("1".to_string(), "2".to_string());
+        let msg = format!("{}", err);
+        assert_eq!(msg.as_str(), "1 - Reason: 2");
+    }
+}
