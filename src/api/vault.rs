a8f4dc15c7c4a518ba986b986b1d75a0396d7faf
Saurav Gupta <saurav2k4u@gmail.com>
2020-02-29 01:05:06 +0800

    Made adjustments for trust_token_bearer and agron2

--- src/api/vault.rs
+++ src/api/vault.rs
@@ -12,17 +12,21 @@ pub trait UserIdentity {
 
 /// Return normally if login succeeds else return an Error
 #[async_trait]
-pub trait UserAuthentication {
+pub trait UserAuthentication: TrustToken {
     /// Implementation Required
     async fn check_user_valid(&mut self, user: &str, password: &str) -> Result<Option<Session>, Error>;
 }
 
+pub trait TrustToken {
+    /// Implementation Required
+    fn trust_token_bearer(&self) -> bool;
+}
+
+
 pub trait PasswordHasher<H: Default> {
     /// Implementation Required
-    fn trust_token(&self) -> bool;
-    fn hash_user_password<T: AsRef<str>>(&self, user: T,  password: T) -> Result<String, Error>;
-    fn verify_user_password<T: AsRef<str>>(&self, user: T,  password: T,  hash: T) -> Result<bool, Error>;
-
+    fn hash_user_password<T: AsRef<str>>(&self, user: T, password: T) -> Result<String, Error>;
+    fn verify_user_password<T: AsRef<str>>(&self, user: T, password: T, hash: T) -> Result<bool, Error>;
 }
 
 /// Workflow for library user
@@ -38,12 +42,11 @@ pub trait Workflow {
 
 pub async fn resolve_session_from_client_authentication_token<H, V>(vault: &mut V, user: &str, token: &str) -> Result<ServerClaims, Error>
     where H: Default + Hasher, V: Store + UserIdentity + UserAuthentication + PersistenceHasher<H> + Persistence {
-    let client_claims = decode_client_token(vault.public_authentication_certificate(), token)?;
-    let user_from_token = String::from_utf8_lossy(client_claims.sub().as_slice()).to_string();
-    let _ = vault.check_same_user(user, user_from_token.as_str()).await?;
+    // Decode client authentication token
     let public_certificate = vault.public_authentication_certificate();
     let claims = decode_client_token(public_certificate, token)?;
 
+    // Get server side token
     let reference = claims.reference();
     let refresh_token_from_store = vault.load(reference).await;
     if refresh_token_from_store.is_none() {
@@ -51,22 +54,35 @@ pub async fn resolve_session_from_client_authentication_token<H, V>(vault: &mut 
         let reason = "Missing Server Refresh Token".to_string();
         return Err(TokenErrors::MissingServerRefreshToken(msg, reason).into());
     };
+
+    // Decode server side token
+    let public_certificate = vault.public_refresh_certificate();
     let refresh_token_from_store = refresh_token_from_store.unwrap();
-
-    let digest_reference = resolve_authentication_reference::<_, H>(user.as_bytes());
-    let authentication_token_from_store = vault.load(digest_reference).await;
-    if let Some(expected) = authentication_token_from_store {
-        let computed = format!("{}", digest::<_, H>(&token.as_bytes()));
-        if expected != &computed {
-            let msg = format!("User: {:?} Reference: {}", user, reference);
-            let reason = format!(r"Invalid Authentication Token Expected: {} Got: {}", expected, computed);
-            return Err(TokenErrors::InvalidClientAuthenticationToken(msg, reason).into());
-        };
-    };
-
-
-    let public_certificate = &vault.public_refresh_certificate();
     let claims = decode_server_token(public_certificate, refresh_token_from_store.as_str())?;
+
+    // Restore server side token
+    let user_from_token = String::from_utf8_lossy(claims.sub()).to_string();
+
+    // Call hook
+    let _ = vault.check_same_user(user, user_from_token.as_str()).await?;
+
+    // Given the user on server side check if the authentication token was valid
+    let user_digest = resolve_authentication_reference::<_, H>(user_from_token.as_bytes());
+    let token_digest_from_store = vault.load(user_digest).await;
+    if token_digest_from_store.is_none() {
+        let msg = format!("User: {:?} has no session information", user_from_token);
+        let reason = format!("User: {:?} has no data in vault", user_from_token);
+        return Err(TokenErrors::MissingServerRefreshToken(msg, reason).into());
+    };
+    let token_digest_from_store = token_digest_from_store.unwrap();
+    let user_token = digest::<_, H>(&token.as_bytes());
+    let user_token = format!("{}", user_token);
+    if token_digest_from_store.as_str() != user_token {
+        let msg = format!("User: {} client authentication token is not valid", user_from_token);
+        let reason = format!("Requested: {} Available: {}", user_token, token_digest_from_store);
+        return Err(TokenErrors::InvalidClientAuthenticationToken(msg, reason).into());
+    }
+
     Ok(claims)
 }
 
@@ -94,12 +110,13 @@ pub async fn resolve_session_from_client_refresh_token<H, V>(vault: &mut V, user
 }
 
 pub async fn continue_login<H, V>(vault: &mut V, user: &str, pass: &str, refresh_token_expiry_in_seconds: Option<i64>, authentication_token_expiry_in_seconds: Option<i64>) -> Result<Token, Error>
-    where H: Default + Hasher, V: Store + UserIdentity + UserAuthentication + PersistenceHasher<H> + Persistence  {
+    where H: Default + Hasher, V: Store + UserIdentity + UserAuthentication + PersistenceHasher<H> + Persistence + TrustToken {
     let session = vault.check_user_valid(user, pass).await?;
     let (client, server) = match session {
         Some(s) => (s.client, s.server),
         None => (None, None)
     };
+
 
     // Prepare: Token params
     let iat = compute_timestamp_in_seconds();
@@ -122,9 +139,17 @@ pub async fn continue_login<H, V>(vault: &mut V, user: &str, pass: &str, refresh
 
     // Prepare: Client Authentication Token
     let exp = compute_authentication_token_expiry(Some(iat), authentication_token_expiry_in_seconds);
-    let client_authentication_token = prepare_user_authentication_token(
-        vault.private_authentication_certificate(), user, reference, iat, nbf, exp, client.clone(),
-    )?;
+    let client_authentication_token = if vault.trust_token_bearer() {
+        prepare_user_authentication_token(
+            vault.private_authentication_certificate(), user, reference, iat, nbf, exp, client.clone(),
+        )
+    } else {
+        let u = Vec::<u8>::new();
+        prepare_user_authentication_token(
+            vault.private_authentication_certificate(), u.as_slice(), reference, iat, nbf, exp, client.clone(),
+        )
+    }?;
+
 
     let digest_reference = resolve_authentication_reference::<_, H>(user.as_bytes());
     let digest_payload = format!("{}", digest::<_, H>(&client_authentication_token.as_bytes()));
@@ -155,9 +180,17 @@ pub async fn continue_renew<H, V>(vault: &mut V, user: &str, client_refresh_toke
         None => None
     };
 
-    let authentication_token = prepare_user_authentication_token(
-        vault.private_authentication_certificate(), user, reference, iat, nbf, exp, client,
-    )?;
+    let authentication_token = if vault.trust_token_bearer() {
+        prepare_user_authentication_token(
+            vault.private_authentication_certificate(), user, reference, iat, nbf, exp, client.clone(),
+        )
+    } else {
+        let u = Vec::<u8>::new();
+        prepare_user_authentication_token(
+            vault.private_authentication_certificate(), u.as_slice(), reference, iat, nbf, exp, client.clone(),
+        )
+    }?;
+
 
     let digest_reference = resolve_authentication_reference::<_, H>(user.as_bytes());
     let digest_payload = format!("{}", digest::<_, H>(&authentication_token.as_bytes()));
@@ -177,11 +210,7 @@ pub async fn continue_renew<H, V>(vault: &mut V, user: &str, client_refresh_toke
 
 pub async fn continue_logout<H, V>(vault: &mut V, user: &str, client_authentication_token: &String) -> Result<(), Error>
     where H: Default + Hasher, V: Store + UserIdentity + UserAuthentication + PersistenceHasher<H> + Persistence {
-    let claims = decode_client_token(
-        vault.public_authentication_certificate(), client_authentication_token,
-    )?;
-    let user_from_token = String::from_utf8_lossy(claims.sub()).to_string();
-    vault.check_same_user(user, user_from_token.as_str()).await?;
+    let claims = resolve_session_from_client_authentication_token(vault, user, client_authentication_token).await?;
     let reference = claims.reference();
     let result = vault.load(reference).await;
     if result.is_none() {
@@ -189,7 +218,11 @@ pub async fn continue_logout<H, V>(vault: &mut V, user: &str, client_authenticat
         let reason = "Authentication Token not found".to_string();
         return Err(TokenErrors::InvalidClientAuthenticationToken(msg, reason).into());
     };
-    vault.remove(reference).await;
+
+    let _ = vault.remove(reference).await;
+    let key = digest::<_, H>(user.as_bytes());
+    let _ = vault.remove(key).await;
+
     Ok(())
 }
 
