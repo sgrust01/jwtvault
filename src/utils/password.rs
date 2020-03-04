6255a1e94b611f9cb34a71e9621922000750d337
Saurav Gupta <saurav2k4u@gmail.com>
2020-03-04 22:30:30 +0800

    Added struct support for Argon hasher

---
+++ src/utils/password.rs
@@ -0,0 +1,45 @@
+use std::ops::Deref;
+
+use crate::prelude::*;
+
+pub struct ArgonPasswordHasher{
+    secret_key: String
+}
+
+impl Deref for ArgonPasswordHasher {
+    type Target = str;
+    fn deref(&self) -> &Self::Target {
+        self.secret_key.as_str()
+    }
+}
+
+impl ArgonPasswordHasher {
+    pub fn new<T: AsRef<str>>(secret_key: T) -> Self {
+        let secret_key = secret_key.as_ref().to_string();
+        Self { secret_key }
+    }
+}
+
+impl<'a> PasswordHasher<ArgonHasher<'a>> for ArgonPasswordHasher {
+    fn hash_user_password<T: AsRef<str>>(&self, user: T, password: T) -> Result<String, Error> {
+        let secret_key = self.secret_key.as_str();
+        let result = hash_password_with_argon(password.as_ref(), secret_key.as_ref()).map_err(|e| {
+            let msg = format!("Login failed for user: {}", user.as_ref());
+            let reason = e.to_string();
+            LoginFailed::PasswordHashingFailed(msg, reason).into()
+        });
+        result
+    }
+    fn verify_user_password<T: AsRef<str>>(&self, user: T, password: T, hash: T) -> Result<bool, Error> {
+        let secret_key = self.secret_key.as_str();
+        let result = verify_user_password_with_argon(
+            password.as_ref(), secret_key.as_ref(), hash.as_ref(),
+        ).map_err(|e| {
+            let reason = e.to_string();
+            let msg = format!("Login verification for user: {} Reason: {}", user.as_ref(), reason);
+            let reason = e.to_string();
+            LoginFailed::PasswordVerificationFailed(msg, reason).into()
+        });
+        result
+    }
+}
